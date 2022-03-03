84160fe0e4569e70cf6333b243e6ab55554feabc
Yumechi756 <derekmhkwok@hotmail.com>
2022-03-03 23:08:25 +0800

    Initial Commit

---
+++ StreamingLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreamingLab
+//
+//  Created by Yumechi on 3/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
